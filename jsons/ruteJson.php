299d0a3fc5c88899ec09dd4b5653eab0cc784484
Joan <joanandreusole@gmail.com>
2019-12-04 00:04:14 +0100

    Ajax

--- jsons/ruteJson.php
+++ jsons/ruteJson.php
@@ -3,8 +3,9 @@ include 'jsons.php';
 if(isset($_GET['name'])) {
     $and = "and dificultat.name = '" . $_GET['name'] . "'";
     echo getRutesJson($and);
+}else if(isset($_GET['id'])) {
+    $and = "and rute.id = " . $_GET['id'];
+    echo getRutesJson($and);
 }else{
     echo getRutesJson();
 }
-
-
